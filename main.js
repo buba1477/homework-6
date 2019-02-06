5372451b28335c385add28d3abfb7a6d487fe0da
Amr <abazov1477@gmail.com>
2019-02-06 22:32:36 +0300

    1

---
+++ main.js
@@ -0,0 +1,13 @@
+//Реализовать функцию getSevenCol, которая принимает на вход массив чисел, а возвращает число с подсчитанным количеством чисел 7.
+function getSevenCol (a) {
+var sum = 0;
+var b = a.join('').split('');
+for (var i = 0; i < b.length; i++){
+if (b[i]==7) {
+sum+=1;
+           }
+         }
+return sum;
+   }
+
+console.log (getSevenCol([1, 7, 4, 77, 73])); 
